b3d5e2ec9632d3d1f0c2469583c9b2188617affb
Jim Laney <jlaney@uw.edu>
2020-05-22 12:23:56 -0700

    adds the host to section_url for correct link display, and refactor the json parsing to the model

--- uw_gradepage/grading_status.py
+++ uw_gradepage/grading_status.py
@@ -1,22 +1,18 @@
-
 """
 This is the interface for interacting with the GradePage Web Service.
 """
 
-from dateutil.parser import parse
-from logging import getLogger
 from uw_gradepage.models import GradingStatus
 from uw_gradepage import get_resource
 from urllib.parse import quote
 
 GRADING_STATUS_URL = "/api/v1/grading_status/{section_id}"
-logger = getLogger(__name__)
 
 
 def get_grading_status(section_id, act_as=None):
     """
     Return a restclients.models.gradepage.GradePageStatus object
-    on the given course
+    for the given course
     """
     url = GRADING_STATUS_URL.format(section_id=quote(section_id))
     headers = {}
@@ -24,28 +20,4 @@ def get_grading_status(section_id, act_as=None):
     if act_as is not None:
         headers["X-UW-Act-as"] = act_as
 
-    return _object_from_json(url, get_resource(url, headers))
-
-
-def _object_from_json(url, json_data):
-    return_obj = GradingStatus()
-
-    gs_data = json_data.get('grading_status')
-    return_obj.section_id = gs_data['section_id']
-    return_obj.grading_period_open = gs_data['grading_period_open']
-    return_obj.display_name = gs_data['display_name']
-    return_obj.section_url = gs_data['section_url']
-    return_obj.status_url = gs_data['status_url']
-    return_obj.grading_status = gs_data['grading_status']
-
-    try:
-        return_obj.submitted_count = gs_data['submitted_count']
-        return_obj.unsubmitted_count = gs_data['unsubmitted_count']
-        return_obj.submitted_by = gs_data['submitted_by']
-        return_obj.submitted_date = parse(gs_data["submitted_date"])
-        return_obj.accepted_date = parse(gs_data["accepted_date"])
-        return_obj.no_grades_submitted = False
-    except KeyError:
-        return_obj.no_grades_submitted = True
-
-    return return_obj
+    return GradingStatus.from_json(get_resource(url, headers))
--- uw_gradepage/tests/test_grading_status.py
+++ uw_gradepage/tests/test_grading_status.py
@@ -1,33 +1,41 @@
 from unittest import TestCase
 from datetime import datetime
 from dateutil.tz import tzutc
+from commonconf import override_settings
 from uw_gradepage.grading_status import get_grading_status
 
 
+@override_settings(RESTCLIENTS_GRADEPAGE_HOST='https://gradepage.test.edu')
 class TestGradingStatus(TestCase):
     def test_get_grading_status_submitted(self):
         gs = get_grading_status(
             '2013-spring-A A-123-AA-FBB38FE46A7C11D5A4AE0004AC494FFE',
             act_as="bill")
-        self.assertEquals(gs.no_grades_submitted, False)
-        self.assertEquals(
+        self.assertEqual(gs.no_grades_submitted, False)
+        self.assertEqual(
             gs.section_id,
             '2013-spring-A A-123-AA-FBB38FE46A7C11D5A4AE0004AC494FFE')
-        self.assertEquals(gs.display_name, 'A A 123 AA')
-        self.assertEquals(gs.grading_status, None)
-        self.assertEquals(gs.submitted_date,
-                          datetime(2013, 6, 6, 21, 42, 19, tzinfo=tzutc()))
-        self.assertEquals(gs.accepted_date,
-                          datetime(2013, 6, 6, 21, 42, 20, tzinfo=tzutc()))
+        self.assertEqual(gs.display_name, 'A A 123 AA')
+        self.assertEqual(gs.grading_status, None)
+        self.assertEqual(gs.submitted_date,
+                         datetime(2013, 6, 6, 21, 42, 19, tzinfo=tzutc()))
+        self.assertEqual(gs.accepted_date,
+                         datetime(2013, 6, 6, 21, 42, 20, tzinfo=tzutc()))
+        self.assertEqual(gs.section_url, (
+            'https://gradepage.test.edu/section/2013-spring-A%20A-123-AA-'
+            'FBB38FE46A7C11D5A4AE0004AC494FFE'))
 
     def test_get_grading_status_not_submitted(self):
         gs = get_grading_status(
             '2013-spring-TRAIN-100-A-FBB38FE46A7C11D5A4AE0004AC494FFE',
             act_as="bill")
-        self.assertEquals(gs.no_grades_submitted, True)
-        self.assertEquals(
+        self.assertEqual(gs.no_grades_submitted, True)
+        self.assertEqual(
             gs.section_id,
             '2013-spring-TRAIN-100-A-FBB38FE46A7C11D5A4AE0004AC494FFE')
-        self.assertEquals(gs.display_name, 'TRAIN 100 A')
-        self.assertEquals(gs.grading_status,
-                          'No submission information available')
+        self.assertEqual(gs.display_name, 'TRAIN 100 A')
+        self.assertEqual(gs.grading_status,
+                         'No submission information available')
+        self.assertEqual(gs.section_url, (
+            'https://gradepage.test.edu/section/2013-spring-TRAIN-100-A-'
+            'FBB38FE46A7C11D5A4AE0004AC494FFE'))
