d721c2220375fb916bdca2778d56f309b4dd4e44
Jim Laney <jlaney@uw.edu>
2021-05-06 09:40:24 -0700

    moves to gihub-actions

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ See the README on `GitHub
 <https://github.com/uw-it-aca/uw-restclients-gradepage>`_.
 """
 
-# The VERSION file is created by travis-ci, based on the tag name
 version_path = 'uw_gradepage/VERSION'
 VERSION = open(os.path.join(os.path.dirname(__file__), version_path)).read()
 VERSION = VERSION.replace("\n", "")
